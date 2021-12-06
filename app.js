a657c6198853bd19a1add00acc9bf1e772d21a31
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-12-06 16:58:09 -0500

    changing sounds ok

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ class DrumKit {
     constructor() {
         this.pads = document.querySelectorAll('.pad');
         this.playBtn = document.querySelector('.play')
+        this.currentKick = './sounds/kick-classic.wav';
+        this.currentSnare = './sounds/snare-acoustic01.wav';
+        this.currentHihat = './sounds/hihat-acoustic01.wav';
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index=0;
         this.bpm = 150;
+        this.isPlaying = null;
+        this.selects = document.querySelectorAll("select");
     }
     activePad(){
         this.classList.toggle('active')
@@ -42,14 +47,47 @@ class DrumKit {
     }
     start(){
         const interval = (60/this.bpm) * 1000;
-        setInterval(() => {
+        //check if it's playing
+        if (!this.isPlaying) {
+        this.isPlaying = setInterval(() => {
             this.repeat()
         }, interval);
-    }
-}
+    } else {
+        //clear the interval
+        clearInterval(this.isPlaying);
+        this.isPlaying = null;
 
+        }
+    }
+    updateBtn() {
+        if (!this.isPlaying) {
+            this.playBtn.innerText = "Stop"
+            this.playBtn.classList.add('active');
+        } else {
+            this.playBtn.innerText = "Play"
+            this.playBtn.classList.remove('active');
+        }
+    }
+    changeSound(e) {
+        const selectionName = e.target.name;
+        const selectionValue = e.target.value;
+        switch (selectionName) {
+          case "kick-select":
+            this.kickAudio.src = selectionValue;
+            break;
+          case "snare-select":
+            this.snareAudio.src = selectionValue;
+            break;
+          case "hihat-select":
+            this.hihatAudio.src = selectionValue;
+            break;
+    }
+    
+}
+}
 const drumKit = new DrumKit();
 
+//Event Listeners
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
@@ -59,5 +97,12 @@ drumKit.pads.forEach(pad => {
 });
 
 drumKit.playBtn.addEventListener('click', () => {
+    drumKit.updateBtn();
     drumKit.start();
 });
+
+drumKit.selects.forEach(select => {
+    select.addEventListener("change", function(e) {
+      drumKit.changeSound(e);
+    });
+  });
